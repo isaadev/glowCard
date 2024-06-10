45394ad2576cb14d17c5bd3d3a9854e3f37677cf
i <101498370+isaadev@users.noreply.github.com>
2024-06-10 18:33:15 -0400

    button changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import './App.css'
-
+import "./App.css";
+import cartIcon from "./cart.svg";
 
 function BasicExample() {
   return (
@@ -17,11 +18,17 @@ function BasicExample() {
         <div className="price place-self-end">$15</div>
       </div>
       <Card.Img variant="top" src="holder.js/100px180" className="" />
-      <Card.Body>
-        <Card.Title className="product-text p-2">Item 1</Card.Title>
-        <Card.Text className="seller-text p-1">Seller: Jinwoo</Card.Text>
-        <Button variant="primary" className="add-to-cart-btn-text mt-2">
-          (ic) Add to Cart
+      <Card.Body className="flex flex-col items-center justify-between">
+        <div>
+          <Card.Title className="product-text p-2">Item 1</Card.Title>
+          <Card.Text className="seller-text p-1">Seller: Jinwoo</Card.Text>
+        </div>
+        <Button
+          variant="primary"
+          className="add-to-cart-btn-text mt-2 p-3 flex items-center"
+        >
+          <img src={cartIcon} alt="Cart" className="cart-icon mr-2" />
+          Add to Cart
         </Button>
       </Card.Body>
     </Card>
@@ -29,7 +36,6 @@ function BasicExample() {
 }
 
 function App() {
-
   return (
     <>
       <head>
@@ -38,12 +44,11 @@ function App() {
           rel="stylesheet"
         />
       </head>
-      <div>
+      <div className="flex items-center justify-center ">
         <BasicExample />
-
       </div>
     </>
   );
 }
 
-export default App
+export default App;
