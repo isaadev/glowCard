0863474c408e64577f29bde71b6787d064f3c792
T0raT <tigerliu@bu.edu>
2024-06-11 21:45:57 -0400

    page strucutre is mostly done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,82 @@ import "./App.css";
 import ProductCard from "./components/ProductCard.jsx";
 
 
+// card
+const Card = () => {
+  return (
+    <div className="
+    card rounded-3xl
+    flex flex-col justify-between
+    min-h-cardHeight max-h-cardHeight min-w-cardwidth p-cardpad
+     ">
+      <div id="top-text" className="flex flex-row justify-between text-topTxt">
+        <div id="warrant-status" className="">Warranted</div>
+        <div id="productPrice" className="">$15</div>
+      </div>
+      <img src=""/>
+      <div id="product-info" className="items-center text-center">
+        <div id="productName" className="p-0.5 text-prodTxt">Leather Bag</div>
+        <div id="sellerName" className="p-0.5 text-sellerTxt">Seller: Satoru</div>
+        <button className="rounded-2xl border-2 border-purple-300 p-btnpad text-btnTxt">Add to Cart</button>
+      </div>
+
+
+    </div>
+  )
+}
+
+const ProfileHeader = () => {
+
+  return (
+      <div id='Profile-container'
+           className='
+           border-2 rounded-full
+           min-h-full w-profileW self-center
+           flex justify-between
+           '>
+
+        <div id='profile-left' className="flex items-center justify-center ml-6">
+          <p>Round <span className="p-1.5 bg-fuchsia-600 rounded-full">4/10</span></p>
+
+          <div className="mx-4 h-6 bg-gray-400 w-px"></div>
+
+          <p>&#128176; Budget <span className='p-1.5 bg-fuchsia-600 rounded-full'>$40</span></p>
+        </div>
+
+        <div id='profile-right' className="flex items-center justify-center mr-6">
+          <p>Round <span className="p-1.5 bg-fuchsia-600 rounded-full">1</span></p>
+
+          <div className="mx-4 h-6 bg-gray-400 w-px"></div>
+
+          <p>🕛 Time <span className='p-1.5 bg-fuchsia-600 rounded-full'>00:05:00</span></p>
+        </div>
+      </div>
+  )
+}
+
+// will use grid-rows-3 for cards
+
 const ConsumerProductPage = () => {
 
+  const redBorder = 'border-2 border-red-900'
   return (
       <>
         <div id='body-container' className='h-svh flex flex-col'>
-          <div id="top-div" className='border-2 border-red-900 min-h-top'>top</div>
-          <div id="middle-div" className='border-2 border-red-900 flex-auto min-h-middle'>middle</div>
-          <div id="bottom-div" className='border-2 border-red-900 bottom-0 w-svw min-h-bottom'>bottom</div>
+          <div id="top-div" className={`min-h-top flex justify-center`}>
+            <ProfileHeader />
+          </div>
+          <div id="middle-div" className='flex-auto min-h-middle grid grid-cols-3 grid-rows-2 justify-items-center items-center'>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+          </div>
+          <button id="bottom-div"
+                  className='self-center rounded-2xl
+                  bottom-0 w-40 min-h-bottom
+                  bg-nextbtn text-black'>Next</button>
         </div>
       </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    spacing: {
-        'top': '5rem',
+    extend: {
+      spacing: {
+        'top': '3.5rem',
         'middle': '30rem',
-        'bottom': '5rem',
-        '0' : '0rem'
+        'bottom': '4rem',
+        '0' : '0rem',
+        '0.5' : '0.5rem',
+        'btnpad': '1rem',
+        'cardpad': '1.5rem',
+        'cardwidth': '240px',
+        'cardHeight': '320px',
+        'profileW': '1575px'
+
+      },
+      fontSize: {
+        'prodTxt': '0.8rem',
+        'sellerTxt': '0.6rem',
+        'btnTxt': '0.45rem',
+        'topTxt': '0.7rem',
+      },
+      colors: {
+        'nextbtn': '#DCC7DD'
+      }
     },
+
   },
   plugins: [],
 };
