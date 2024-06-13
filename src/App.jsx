f03619382deee70402792d855cccd0023e8d8c09
T0raT <tigerliu@bu.edu>
2024-06-13 18:46:55 -0400

    more clean ups

--- src/App.jsx
+++ src/App.jsx
@@ -4,47 +4,6 @@ import Instructions from "./components/Instructions.jsx";
 import IntroPage from "./components/IntroPage.jsx";
 
 
-const ProfileHeader = () => {
-  return (
-    <div
-      id="Profile-container"
-      className="
-           border-2 rounded-full
-           min-h-full w-profileW self-center
-           flex justify-between
-           "
-    >
-      <div id="profile-left" className="flex items-center justify-center ml-6">
-        <p>
-          Round <span className="p-1.5 bg-fuchsia-600 rounded-full">4/10</span>
-        </p>
-
-        <div className="mx-4 h-6 bg-gray-400 w-px"></div>
-
-        <p>
-          &#128176; Budget{" "}
-          <span className="p-1.5 bg-fuchsia-600 rounded-full">$40</span>
-        </p>
-      </div>
-
-      <div id="profile-right" className="flex items-center justify-center mr-6">
-        <p>
-          Round <span className="p-1.5 bg-fuchsia-600 rounded-full">1</span>
-        </p>
-
-        <div className="mx-4 h-6 bg-gray-400 w-px"></div>
-
-        <p>
-          🕛 Time{" "}
-          <span className="p-1.5 bg-fuchsia-600 rounded-full">00:05:00</span>
-        </p>
-      </div>
-    </div>
-  );
-};
-
-// will use grid-rows-3 for cards
-
 const ConsumerProductPage = () => {
   const redBorder = "border-2 border-red-900";
   return (
@@ -76,8 +35,6 @@ const ConsumerProductPage = () => {
     </>
   );
 };
-
-
 
 
 function App() {
