92803c2652129190b3b5099e9998b9b1db2d54f6
T0raT <tigerliu@bu.edu>
2024-06-14 16:14:12 -0400

    Slight clean up into components

--- src/App.jsx
+++ src/App.jsx
@@ -4,58 +4,15 @@ import Instructions from "./components/Instructions.jsx";
 import IntroPage from "./components/IntroPage.jsx";
 import ProfileHeader from "./components/ProfileHeader";
 import ProductCard from "./components/ProductCard.jsx";
+import ConsumerProductPage from "./components/ConsumerChoice.jsx";
 // card
-
-
-const ConsumerProductPage = () => {
-
-  return (
-    <>
-      <div id="body-container" className="min-h-screen flex flex-col">
-        <div id="top-div" className={`min-h-top flex justify-center`}>
-          <ProfileHeader />
-        </div>
-
-        <div
-          id="middle-div"
-          className="
-               flex-auto justify-items-center items-center
-               grid grid-cols-1
-               min-h-middle
-
-               desktop:px-52 tablet:px-32
-               tablet:grid-cols-2 desktop:grid-cols-3"
-        >
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-        </div>
-
-        <div className="flex justify-center min-h-bottom tablet:min-h-smBottom">
-          <button
-            id="bottom-div"
-            className="
-              self-center rounded-2xl w-40 text-xs
-              border-2 border-nextbtn text-nextbtn  tablet:w-32 mb-3 py-4 
-            "
-          >
-            Next >
-          </button>
-        </div>
-      </div>
-    </>
-  );
-};
 
 
 function App() {
   return (
     <>
-      <IntroPage />
-      <Instructions />
+      {/*<IntroPage />*/}
+      {/*<Instructions />*/}
       <ConsumerProductPage />
     </>
   );
---
+++ src/components/ConsumerChoice.jsx
@@ -0,0 +1,48 @@
+import ProfileHeader from "./ProfileHeader.jsx";
+import ProductCard from "./ProductCard.jsx";
+import React from "react";
+
+const ConsumerProductPage = () => {
+
+  return (
+      <>
+        <div id="body-container" className="min-h-screen flex flex-col">
+          <div id="top-div" className={`min-h-top flex justify-center`}>
+            <ProfileHeader />
+          </div>
+
+          <div
+              id="middle-div"
+              className="
+               flex-auto justify-items-center items-center
+               grid grid-cols-1
+               min-h-middle
+
+               desktop:px-52 tablet:px-32
+               tablet:grid-cols-2 desktop:grid-cols-3"
+          >
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+
+          <div className="flex justify-center min-h-bottom tablet:min-h-smBottom">
+            <button
+                id="bottom-div"
+                className="
+              self-center rounded-2xl w-40 text-xs
+              border-2 border-nextbtn text-nextbtn  tablet:w-32 mb-3 py-4
+            "
+            >
+              Next >
+            </button>
+          </div>
+        </div>
+      </>
+  );
+};
+
+export default ConsumerProductPage
